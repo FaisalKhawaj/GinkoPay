a7f6d9c57133dab3fb023e74022a130dc0218418
FaisalKhawaj <khawajfaisal981@gmail.com>
2021-06-08 00:17:24 +0500

    PreferencesGeneralModal

--- src/screens/Preferences.js
+++ src/screens/Preferences.js
@@ -5,6 +5,7 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import { RadioButton } from 'react-native-paper';
 import { BackgroundColor } from '../constants/colors';
 import Modal from 'react-native-modal';
+import DropDownPicker from 'react-native-dropdown-picker';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { mystyles } from '../styles';
 import BackBtnWithMiddleText from '../components/BackBtnMiddleText';
@@ -25,9 +26,17 @@ const Preferences = ({ navigation }) => {
     const UpdateGeneralHandler = () => {
         alert('Update')
     }
-    const [items, setItems] = useState([
-        { label: 'Best Match', value: 'Best Match' },
-        { label: 'Banana', value: 'banana' },
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [CurrencyDropDown, setCurrencyDropDown] = useState([
+        { label: 'Dollar', value: 'USD-United State Dollar' },
+        { label: 'Euro', value: 'Euro' },
+    ]);
+    const [openLanguage, setOpenLanguage] = useState(false)
+    const [valueLanguage, setValueLanguage] = useState(null)
+    const [LanguageDropDown, setLanguageDropDown] = useState([
+        { label: 'English', value: 'English' },
+        { label: 'Italic', value: 'Italic' },
     ]);
     // const GeneralModalHandler=()=>
     // {
@@ -71,25 +80,37 @@ const Preferences = ({ navigation }) => {
                          </Text>
                         </View>
 
-                        {/* <DropDownPicker
-            open={open}
-            value={value}
-            items={items}
-            searchable={false}
-            placeholder="Top Rated"
-            style={mystyles.dropdown}
-            showArrowIcon={true}
-            arrowIconStyle={{ tintColor: open ? 'green' : '#FFFFFF' }}
-            dropDownContainerStyle={{
-              backgroundColor: '#131313',
-              borderColor: '#272727',
-            }}
-            textStyle={mystyles.fstext}
-            containerStyle={{ width: '40%', height: 44 }}
-            setValue={setValue}
-            setItems={setItems}
-            setOpen={setOpen}
-          /> */}
+                        <DropDownPicker
+                            open={open}
+                            value={value}
+                            items={CurrencyDropDown}
+                            searchable={false}
+                            placeholder="USD-United State Dollar"
+                            style={{
+                                backgroundColor: '#131313',
+                                borderColor: '#17171A',
+                                borderRadius: 10,
+                                // width: wp('90%'),
+                                // alignSelf: 'center'
+                            }}
+                            showArrowIcon={true}
+                            arrowIconStyle={{ tintColor: open ? 'green' : '#FFFFFF' }}
+                            dropDownContainerStyle={{
+                                backgroundColor: '#131313',
+                                borderColor: '#272727',
+                            }}
+                            textStyle={{
+                                fontSize: 15,
+                                fontWeight: '400',
+                                color: '#FFFF',
+                                fontFamily: 'Poppins-Bold',
+
+                            }}
+                            containerStyle={{ width: '90%', alignSelf: 'center', height: 44 }}
+                            setValue={setValue}
+                            setItems={setCurrencyDropDown}
+                            setOpen={setOpen}
+                        />
 
                         <View style={styles.CurrencyPRivacyCurrentLanUserSearchView}>
                             <Text style={styles.headingText}>Privacy Currency</Text>
@@ -120,6 +141,49 @@ const Preferences = ({ navigation }) => {
                                 <Text style={{ alignSelf: 'center', color: '#FFFF' }}>Flat</Text>
                             </View>
                         </View>
+
+
+                        <View style={styles.CurrencyPRivacyCurrentLanUserSearchView}>
+                            <Text style={styles.headingText}>Current Language</Text>
+                            <Text style={styles.descriptionText}>
+                                Translate the application to a different{'\n'}supported language
+                             </Text>
+                        </View>
+
+                        <DropDownPicker
+                            open={openLanguage}
+                            value={valueLanguage}
+                            items={LanguageDropDown}
+                            searchable={false}
+                            placeholder="English"
+                            style={{
+                                backgroundColor: '#131313',
+                                borderColor: '#17171A',
+                                borderRadius: 10,
+                                // width: wp('90%'),
+                                // alignSelf: 'center'
+                            }}
+                            showArrowIcon={true}
+                            arrowIconStyle={{ tintColor: open ? 'green' : '#FFFFFF' }}
+                            dropDownContainerStyle={{
+                                backgroundColor: '#131313',
+                                borderColor: '#272727',
+                            }}
+                            textStyle={{
+                                fontSize: 15,
+                                fontWeight: '400',
+                                color: '#FFFF',
+                                fontFamily: 'Poppins-Bold',
+                            }}
+                            containerStyle={{ width: '90%', alignSelf: 'center', height: 44 }}
+                            setValue={setValueLanguage}
+                            setItems={setLanguageDropDown}
+                            setOpen={setOpenLanguage}
+                        />
+
+
+
+
 
                         <View style={styles.CurrencyPRivacyCurrentLanUserSearchView}>
                             <Text style={styles.headingText}>User can search my profile </Text>
